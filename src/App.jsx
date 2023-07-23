4b48c44acbad7d63204ef97e9cdf1e3c0c6aa746
“Jason <jasonrodalez@gmail.com>
2023-07-22 19:23:13 -0500

    Mas actual

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Header from "./components/Header"
 import Form from "./components/Form"
 import List from "./components/List"
+import { useState } from "react"
 
 
 function App() {
-// Este es mi proyectode practicas
-// Y resalta la claridad de mi proyecto
+  const [pacientes, setPacientes] = useState([]);
+
+
   return (
       <div className="container w-10/12 mx-auto mt-2 md:h-screen">
         <Header />
         <div className="md:flex mt-5">
           <Form
+          pacientes={pacientes}
+          setPacientes={setPacientes}
        
           />
           <List
